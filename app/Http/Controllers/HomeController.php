83856613e03ca5fd13f4aca183a59f88233df7a0
alijafari4544 <110987057+alijafari4544@users.noreply.github.com>
2024-05-15 19:10:56 +0330

    create Home Controller and create dasboard admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    function home(){
-        return 'kion';
+    public function index(){
+        if(Auth::id()){
+            $userType=Auth()->user()->user_type;
+            if($userType=='user'){
+                return view('dashboard');
+            }elseif ($userType=='admin'){
+                return  view('admin.adminHome');
+            }
+        }
+        else{
+            return  redirect()->back();
+        }
     }
 }
